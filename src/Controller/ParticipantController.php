8fb8b6f9077fd5b28f00da4a78e3e713d35fb003
Amandine Foulet <amandine.foulet@gmail.com>
2020-07-06 16:19:29 +0200

    creation of login
    controller+twig+security

---
+++ src/Controller/ParticipantController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+
+
+use App\Entity\Participants;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class ParticipantController extends AbstractController
+{
+
+}
