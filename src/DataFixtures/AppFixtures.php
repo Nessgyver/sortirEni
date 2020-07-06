c8b00bbb1c055a5029b28f828c1b36e3cc541d68
Amandine Foulet <amandine.foulet@gmail.com>
2020-07-06 12:08:53 +0200

    Add fixtures +faker for Participants, Campus & Etats

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,40 +4,63 @@ namespace App\DataFixtures;
 
 use App\Entity\Campus;
 use App\Entity\Etats;
+use App\Entity\Participants;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $faker = \Faker\Factory::create();
+        //création des participants
+        for ($i = 0; $i < 10; $i++) {
+            $participant = new Participants();
+            $participant->setUsername($faker->userName);
+            $participant->setNom($faker->lastname);
+            $participant->setPrenom($faker->firstNameMale);
+            $participant->setTelephone($faker->e164PhoneNumber);
+            $participant->setMail($faker->email);
+            $participant->setPassword($faker->passWord);
+            $participant->setRoles(["ROLE_USER"]);
+            $participant->setAdministrateur(0);
+            $participant->setActif(1);
+            $participant->setCampusNoCampus($faker->numberBetween(1, 3));
+            $manager->persist($participant);
+        }
+
         //création des états pour les sorties
-        $this->loadEtats();
+        $cree = new Etats();
+        $cree->setLibelle("Créée");
+        $manager->persist($cree);
+        $ouverte = new Etats();
+        $ouverte->setLibelle("Ouverte");
+        $manager->persist($ouverte);
+        $cloturee = new Etats();
+        $cloturee->setLibelle("Cloturée");
+        $manager->persist($cloturee);
+        $enCours = new Etats();
+        $enCours->setLibelle("Activité en cours");
+        $manager->persist($enCours);
+        $passee = new Etats();
+        $passee->setLibelle("Passée");
+        $manager->persist($passee);
+        $annulee = new Etats();
+        $annulee->setLibelle("Annulée");
+        $manager->persist($annulee);
 
         //création des campus
         $nantes = new Campus();
         $nantes->setNomCampus("Nantes");
+        $manager->persist($nantes);
         $rennes = new Campus();
         $rennes->setNomCampus("Rennes");
+        $manager->persist($rennes);
         $niort = new Campus();
         $niort->setNomCampus("Niort");
+        $manager->persist($niort);
 
         $manager->flush();
     }
 
-    public function loadEtats(): void
-    {
-        $cree = new Etats();
-        $cree->setLibelle("Créée");
-        $ouverte = new Etats();
-        $ouverte->setLibelle("Ouverte");
-        $cloturee = new Etats();
-        $cloturee->setLibelle("Cloturée");
-        $enCours = new Etats();
-        $enCours->setLibelle("Activité en cours");
-        $passee = new Etats();
-        $passee->setLibelle("Passée");
-        $annulee = new Etats();
-        $annulee->setLibelle("Annulée");
-    }
 }
