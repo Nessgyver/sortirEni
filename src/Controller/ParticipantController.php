5a8a190a133142a947761b53f4da9d8511646035
Amandine <amandine.foulet@gmail.com>
2020-07-07 15:29:31 +0200

    Fix update session password after modif

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -7,10 +7,13 @@ namespace App\Controller;
 use App\Entity\Participant;
 use App\Form\ParticipantType;
 use App\Repository\ParticipantRepository;
+use App\Security\LoginAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 
 
 class ParticipantController extends AbstractController
@@ -18,7 +21,7 @@ class ParticipantController extends AbstractController
     /**
      * @Route("/profil/{id}", name="profil", methods={"GET", "POST"})
      */
-    public function showProfile($id, ParticipantRepository $partiRepo, Request $request, EntityManagerInterface $em) {
+    public function showProfile($id, ParticipantRepository $partiRepo, Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $encoder, GuardAuthenticatorHandler $guardHandler, LoginAuthenticator $authenticator) {
 
         //Affichage du profil du user connecté
         $participantCo = $this->getUser();
@@ -30,10 +33,19 @@ class ParticipantController extends AbstractController
             $form = $this->createForm(ParticipantType::class, $participantCo);
             $form->handleRequest($request);
             if ($form->isSubmitted() && $form->isValid()) {
-                $em->merge($participantCo);
+                $password = $participantCo->getPassword();
+                $encodedPassword= $encoder->encodePassword($participantCo, $password);
+                $participantCo->setPassword($encodedPassword);
+                $em->persist($participantCo);
                 $em->flush();
 
                 $this->addFlash("success", "Profil mis à jour");
+                return $guardHandler->authenticateUserAndHandleSuccess(
+                    $participantCo,
+                    $request,
+                    $authenticator,
+                    'main' // firewall name in security.yaml
+                );
             }
 
             return $this->render('participant/profil.html.twig', ['participantCo'=>$participantCo, 'form' => $form->createView(), 'id' => $id]);
