003291b7619831cdb0f67d525931b1670b17de37
Amandine <amandine.foulet@gmail.com>
2020-07-07 14:04:03 +0200

    create profile (show and modify)
    twig+form+controller

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -5,11 +5,48 @@ namespace App\Controller;
 
 
 use App\Entity\Participant;
+use App\Form\ParticipantType;
+use App\Repository\ParticipantRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 
 class ParticipantController extends AbstractController
 {
+    /**
+     * @Route("/profil/{id}", name="profil", methods={"GET", "POST"})
+     */
+    public function showProfile($id, ParticipantRepository $partiRepo, Request $request, EntityManagerInterface $em) {
+
+        //Affichage du profil du user connecté
+        $participantCo = $this->getUser();
+        if($participantCo->getId()==$id) {
+            //Recherche du participant connecté
+            $participantCo = $partiRepo->find($id);
+
+            //Création du formulaire permettant la modification de profil
+            $form = $this->createForm(ParticipantType::class, $participantCo);
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $em->merge($participantCo);
+                $em->flush();
+
+                $this->addFlash("success", "Profil mis à jour");
+            }
+
+            return $this->render('participant/profil.html.twig', ['participantCo'=>$participantCo, 'form' => $form->createView(), 'id' => $id]);
+        }
+
+        //
+        else if($participantCo->getId()!=$id){
+            $participantLambda = $partiRepo->find($id);
+
+            return $this->render('participant/profil.html.twig', ['participantLambda'=>$participantLambda, 'id' => $id]);
+        }
+
+
+    }
 
 }
---
+++ src/Form/ParticipantType.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Participant;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ParticipantType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class, [
+                'label'=>'Pseudo: '
+            ])
+            ->add('prenom', TextType::class, [
+                'label'=>'Prénom: '
+            ])
+            ->add('nom', TextType::class, [
+                'label'=>'Nom: '
+            ])
+            ->add('telephone', TextType::class, [
+                'label'=>'Téléphone: '
+            ])
+            ->add('mail', EmailType::class, [
+                'label'=>'Email: '
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passe doivent correspondre.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'Mot de passe: '],
+                'second_options' => ['label' => 'Confirmation: '],
+            ])
+            ->add("campus", null,[
+                'label' => "Campus: ",
+                'choice_label' => "nom",
+                'query_builder' => function(EntityRepository $repo){
+                    return $repo->createQueryBuilder('c')->addOrderBy('c.nom', 'ASC');
+                }
+            ])
+           
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Participant::class,
+        ]);
+    }
+}
