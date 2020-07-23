d580d50161e4d2d7faa1338e85c1065bc0d98fa7
Loisel <loiselmathieu35@gmail.Com>
2020-07-23 23:37:39 +0200

    fix multiple registrations to same sortie

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -7,6 +7,7 @@ use App\Entity\Sortie;
 use App\Repository\EtatRepository;
 use App\Repository\InscriptionRepository;
 use App\Repository\SortieRepository;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -44,12 +45,23 @@ class InscriptionController extends AbstractController
     /**
      * @Route("/inscrire/{id}", name="inscrire")
      */
-    public function inscrire(int $id, SortieRepository $sortieRepository, EntityManagerInterface $entityManager, EtatRepository $etatRepository)
+    public function inscrire($id, SortieRepository $sortieRepository, EntityManagerInterface $entityManager, EtatRepository $etatRepository, InscriptionRepository $inscriptionRepository)
     {
-        //Création de la route et récupération de l'id
+        //Création de la route
         $route = 'sortie_afficher';
+
+        //Récupération de l'utilisateur courant
+        $currentUser = $this->getUser();
+
+        //Récupération de la sortie
         $sortie = $sortieRepository->findOneBy([
             'id' => $id
+        ]);
+
+        //Récupération de l'inscription éventuelle du participant courant à la sortie
+        $participantIsInscrit = $inscriptionRepository->findBy([
+            'sortie' => $id,
+            'participant' => $currentUser,
         ]);
 
         //Si le nombre maximum d'inscriptions est atteint, l'état de la sortie passe sur close
@@ -62,16 +74,23 @@ class InscriptionController extends AbstractController
             $this->addFlash('error', 'Nombre maximum d\'inscrit atteint');
         }
 
-        //Création et hydratation de l'inscription à insérer en base
-        $inscription = new Inscription();
-        $currentUser = $this->getUser();
-        $inscription->setParticipant($currentUser);
-        $inscription->setDateInscription(new \DateTime());
-        $inscription->setSortie($sortie);
+        //Si participantIsInscrit est null, alors le participant n'est pas encore inscrit à la sortie
+        if ($participantIsInscrit == null)
+        {
+            //Création et hydratation de l'inscription à insérer en base
+            $inscription = new Inscription();
+            $inscription->setParticipant($currentUser);
+            $inscription->setDateInscription(new DateTime());
+            $inscription->setSortie($sortie);
 
-        //Insertion en base
-        $entityManager->persist($inscription);
-        $entityManager->flush();
+            //Insertion en base
+            $entityManager->persist($inscription);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Vous êtes à présent inscrit à cette sortie');
+        } else {
+            $this->addFlash('error', 'Vous êtes déjà inscrit à cette sortie');
+        }
 
         //renvoie vers la page afficher_sortie
         return $this->redirectToRoute($route, [
